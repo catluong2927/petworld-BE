68b9a28a2bd77da96bbb06fa0ba6386c18a622da
itachi2927 <nguyengiacatluong92@gmail.com>
2023-05-22 09:41:55 +0700

    update + fix issue login

--- src/main/java/com/petworld/dto/userDto/response/UserDtoResponse.java
+++ src/main/java/com/petworld/dto/userDto/response/UserDtoResponse.java
@@ -10,7 +10,6 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDtoResponse {
-    private Long id;
     private String fullName;
     private String userName;
     private String email;
