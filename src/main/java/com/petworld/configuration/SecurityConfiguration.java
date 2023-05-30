fffccf68a954dc3b95ff2108ffed4f510d16ab77
vanminhok12 <vanminhok12@gmail.com>
2023-05-30 15:45:27 +0700

    Add DB and Create new Cart and new Favorite when creating new Customer

--- src/main/java/com/petworld/configuration/SecurityConfiguration.java
+++ src/main/java/com/petworld/configuration/SecurityConfiguration.java
@@ -89,8 +89,8 @@ public class SecurityConfiguration {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         http.authorizeHttpRequests() // links start with /api/
-                .antMatchers("/api/auth/**","/api/product/**","/api/package-details/**", "/api/categorys/**",
-                        "/api/orders/**")// perform segregate authorize
+                .antMatchers("/api/auth/**","/api/products/**","/api/package-details/**", "/api/categorys/**",
+                        "/api/orders/**","/api/cart/**")// perform segregate authorize
                 .permitAll();
 
         // Pages require login with role: ROLE_ADMIN.
